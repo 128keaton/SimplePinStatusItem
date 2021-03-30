95f2d63d52061a99033b98f43523f66a86262e0c
Keaton Burleson <keaton.burleson@me.com>
2021-03-30 15:53:48 -0500

    Initial Commit

---
+++ SimplePinStatusItem/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SimplePinStatusItem
+//
+//  Created by Keaton Burleson on 3/30/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SimplePinStatusItem/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SimplePinStatusItem
+//
+//  Created by Keaton Burleson on 3/30/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
